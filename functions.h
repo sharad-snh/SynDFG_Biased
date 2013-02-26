96e2511db3e8bbefc51d5d72df869fe810d25c70
sharad444 <sharad_sinha@pmail.ntu.edu.sg>
2013-02-27 00:05:48 +0800

    functions.h header file
    Contains the declaration of functions called by main.c

---
+++ functions.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   functions.h
+ * Author: Sharad Sinha
+ * Email: sharad_sinha@pmail.ntu.edu.sg
+ * Rev: 1.0
+ * Release Date: July 31,2012 (Rev 1.0)
+ * © Center for High Performance Embedded Systems(CHiPES),
+ * N4-02A-32,School of Computer Engineering, Nanyang Avenue
+ * Nanyang Technological University
+ * Singapore, 639798
+ * This software is released without any warranty and CHiPES and
+ * the author disclaims all liabilities arising out of its usage
+ */
+
+#ifndef FUNCTIONS_H
+#define  FUNCTIONS_H
+int randomNumGen(int, int);
+int strindex(char s[], char t[]);
+#endif	/* FUNCTIONS_H */
+
